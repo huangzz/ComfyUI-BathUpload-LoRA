b261aeb11931e155dd83ebcd44a2f32b365c52b3
huangzz <huangzz2008@163.com>
2025-02-23 15:50:00 +0800

    2

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from .nodes import PrintInfo
+from .nodes import LoraBatchUploader
 
 # 定义 NODE_CLASS_MAPPINGS 字典，用于 ComfyUI 识别节点
 NODE_CLASS_MAPPINGS = {
-    "PrintInfo": PrintInfo
+    "LoraBatchUploader": LoraBatchUploader
 }
 
 # 定义 NODE_DISPLAY_NAME_MAPPINGS 字典，用于在 ComfyUI 界面显示节点名称
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PrintInfo": "Print Info"
+    "LoraBatchUploader": "Batch Upload LoRA to COS"
 }
